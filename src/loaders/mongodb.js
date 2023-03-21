5ce03e1c4d5a6d7a01df065a26e90119b69d5b79
Cristhyam Augusto <102738785+Cristhyam-Augusto@users.noreply.github.com>
2023-03-21 00:39:21 -0300

    Change mongodb credencials

--- src/loaders/mongodb.js
+++ src/loaders/mongodb.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function startDB() {
   await mongoose.connect(
-    "mongodb+srv://CrisAugusto:Odilonmelhorprofessor@cluster0.zqw2vbl.mongodb.net/test"
+    "mongodb+srv://CrisAugusto:@cluster0.zqw2vbl.mongodb.net/test"
   );
 }
 
